cc39299370aa2039eb5d3a44d5a449ea31c1856f
Niki <nikniksham@gmail.com>
2020-12-24 19:03:02 +0300

    CreateProject

---
+++ Assets/Scripts/Del.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Del : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (transform.position.y <= -30) { Destroy(gameObject); }
+    }
+}
---
+++ Assets/Scripts/SpawnObj.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObj : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+    [SerializeField] GameObject player;
+    [SerializeField] Camera camera;
+
+    public void summon() {        
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(camera.transform.forward);
+        if (Physics.Raycast(ray, out hit)) { Instantiate(prefab, new Vector3(hit.transform.position.x, hit.transform.position.y + 3, hit.transform.position.z), prefab.transform.rotation);}}
+}
